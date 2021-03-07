f09e57b1d1323aa1bb6db8d6acf208c5748abf8d
kduxwr <58582197+kduxwr@users.noreply.github.com>
2021-03-08 03:20:59 +0900

    4

---
+++ runwith/js/top.js
@@ -0,0 +1,13 @@
+const provider = new firebase.auth.GoogleAuthProvider();
+
+$('.login-button').on('click', function() {
+    firebase.auth().signInWithPopup(provider).then(result => {
+        // GoogleプロパイダのOAuthトークンを取得します。
+        const token = result.credential.accessToken;
+        // ログインしたユーザーの情報を取得します。
+        const user = result.user;
+    }).catch(function(err) {
+        console.error(err);
+        // エラー処理
+    });
+});
