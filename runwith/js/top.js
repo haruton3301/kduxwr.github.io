bf4c211a082efe0e4ff687938292a2a8860f013d
kduxwr <58582197+kduxwr@users.noreply.github.com>
2021-03-09 17:52:31 +0900

    1

--- runwith/js/top.js
+++ runwith/js/top.js
@@ -1,3 +1,18 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyCr9eiIN966ucGKJfw4rdo_RncpFvCfbdU",
+    authDomain: "runwith-9be83.firebaseapp.com",
+    projectId: "runwith-9be83",
+    storageBucket: "runwith-9be83.appspot.com",
+    messagingSenderId: "683736651114",
+    appId: "1:683736651114:web:ef9bf56c84355609b78113",
+    measurementId: "G-X1NCY7LC7S"
+    };
+    
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
 $('.login-button').on('click', function() {
@@ -6,6 +21,8 @@ $('.login-button').on('click', function() {
         const token = result.credential.accessToken;
         // ログインしたユーザーの情報を取得します。
         const user = result.user;
+
+        window.location.href = "/home.html";
     }).catch(function(err) {
         console.error(err);
         // エラー処理
