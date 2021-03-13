f3b21095c28fd0f2740bb1cfa0be5f5e0671f59e
kduxwr <58582197+kduxwr@users.noreply.github.com>
2021-03-13 14:36:26 +0900

    11

--- runwith/js/home.js
+++ runwith/js/home.js
@@ -13,28 +13,29 @@ var firebaseConfig = {
     
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
 var userId;
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
         console.log('logged in');
         userId = user.uid;
+
+        db.collection("workouts").where("uid", "==", userId)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
     } else {
         window.location.href = "./index.html";
     } 
   });
 
 
-const db = firebase.firestore();
 
-db.collection("workouts")//.where("uid", "==", userId)
-    .get()
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            // doc.data() is never undefined for query doc snapshots
-            console.log(doc.id, " => ", doc.data());
-        });
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
