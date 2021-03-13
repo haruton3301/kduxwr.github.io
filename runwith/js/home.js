7a5753c886975a0b9e6f0d063a50e271b2550040
kduxwr <58582197+kduxwr@users.noreply.github.com>
2021-03-13 14:53:12 +0900

    1

--- runwith/js/home.js
+++ runwith/js/home.js
@@ -15,6 +15,33 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
+function getStringFromDate(date) {
+ 
+    var year_str = date.getFullYear();
+    //月だけ+1すること
+    var month_str = 1 + date.getMonth();
+    var day_str = date.getDate();
+    var hour_str = date.getHours();
+    var minute_str = date.getMinutes();
+    var second_str = date.getSeconds();
+    
+    // month_str = ('0' + month_str).slice(-2);
+    // day_str = ('0' + day_str).slice(-2);
+    // hour_str = ('0' + hour_str).slice(-2);
+    // minute_str = ('0' + minute_str).slice(-2);
+    // second_str = ('0' + second_str).slice(-2);
+    
+    format_str = 'YYYY年MM月DD hh:mm';
+    format_str = format_str.replace(/YYYY/g, year_str);
+    format_str = format_str.replace(/MM/g, month_str);
+    format_str = format_str.replace(/DD/g, day_str);
+    format_str = format_str.replace(/hh/g, hour_str);
+    format_str = format_str.replace(/mm/g, minute_str);
+    format_str = format_str.replace(/ss/g, second_str);
+    
+    return format_str;
+   };
+
 var userId;
 firebase.auth().onAuthStateChanged(user => {
     if (user) {
@@ -27,7 +54,21 @@ firebase.auth().onAuthStateChanged(user => {
         .get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
+                let data = doc.data();
+                let date = getStringFromDate(data.date);
+                let distance = data.distance;
+                let time = data.time;
+                let speed = data.speed;
+
+                let html = `<div class="workout-child">
+                    <div class="date">` + date + `</div>
+                    <div class="map"></div>
+                    <div class="distance">` + distance + `</div>
+                    <div class="time">` + time + `</div>
+                    <div class="speed">` + speed + `</div>
+                </div>`;
+
+                let child = $(html).appendTo('.workout-list');
                 console.log(doc.id, " => ", doc.data());
             });
         })
