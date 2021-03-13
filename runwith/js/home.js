6a624b07b12bb626a3023a637ffeed47e9b8afd8
kduxwr <58582197+kduxwr@users.noreply.github.com>
2021-03-14 06:25:30 +0900

    n

--- runwith/js/home.js
+++ runwith/js/home.js
@@ -106,7 +106,7 @@ window.onload = function() {
     
                     map.setView({
                         bounds: Microsoft.Maps.LocationRect.fromLocations(coords),
-                        padding: 24 //Add a padding to buffer map to account for pushpin pixel dimensions
+                        padding: 8 //Add a padding to buffer map to account for pushpin pixel dimensions
                     });
                 });
             })
